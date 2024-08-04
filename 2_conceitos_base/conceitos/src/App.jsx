b3590de7d9eee373063401083bc4e89592a0a378
otavio azevedo <otavioazevedo39@gmail.com>
2024-08-04 11:26:46 -0300

    .

--- 2_conceitos_base/conceitos/src/App.jsx
+++ 2_conceitos_base/conceitos/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css';
 // 2 - importando componente
 
 import FirstComponents from './componets/FirstComponents'
+// hierarquia de componentes
+import MyComponents from './componets/MyComponents';
 import TemplateExpression from './componets/template_expression'
 
  function App() {
@@ -13,6 +15,7 @@ import TemplateExpression from './componets/template_expression'
         <h1>Fundamentos do react</h1>
         <FirstComponents/>
         <TemplateExpression/>
+        <MyComponents />
 
         {/*template expression */}
 
---
+++ 2_conceitos_base/conceitos/src/componets/MyComponents.jsx
@@ -0,0 +1,10 @@
+
+const MyComponents = () => {
+  return (
+    <div>
+        <h3>eu estou em varios componentes</h3>
+    </div>
+  )
+}
+
+export default MyComponents
