394f2d4eb71cb8f648aeeb6d90ad0bd427abaf2d
otavio azevedo <otavioazevedo39@gmail.com>
2024-08-04 14:46:10 -0300

    .

--- 2_conceitos_base/my-new-app/src/componets/Events.jsx
+++ 2_conceitos_base/my-new-app/src/componets/Events.jsx
@@ -1,11 +1,19 @@
 // evento de click
 
 const events = () => {
+const Cliquei = () => {
+    console.log("cliquei filho")
+}
+   
   return (
     <div>
         <div>
             <button onClick={() => console.log("testando um evento")}>Clique aqui</button>
         </div>
+        <div>
+            <button onClick={Cliquei}> clique aqui c função</button>
+        </div>
+       
     </div>
   )
 }
