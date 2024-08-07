46fb4d1be6df83db001ce41f935b2600124a8385
otavio azevedo <otavioazevedo39@gmail.com>
2024-08-07 17:10:46 -0300

    .

--- 2_conceitos_base/my-new-app/src/componets/Events.jsx
+++ 2_conceitos_base/my-new-app/src/componets/Events.jsx
@@ -4,6 +4,18 @@ const events = () => {
 const Cliquei = () => {
     console.log("cliquei filho")
 }
+const puteiro = () =>{
+  console.log("partiu zona")
+}
+const renderSomething = (x) =>{
+  if(x) {
+    return <h1>Renderizando isso !!</h1>
+  }else {
+    return <h1>renderizando outra coisa</h1>
+  }
+
+}
+return 10 > 2 && <p>Carregando...</p>
    
   return (
     <div>
@@ -11,8 +23,13 @@ const Cliquei = () => {
             <button onClick={() => console.log("testando um evento")}>Clique aqui</button>
         </div>
         <div>
+          <button onClick={puteiro}>clica aqui no puteiro</button>
+        </div>
+        <div>
             <button onClick={Cliquei}> clique aqui c função</button>
         </div>
+        {renderSomething(true)}
+        {renderSomething(false)}
        
     </div>
   )
